74bf4c3d238a343941ae2801587cde28f73d1cbf
FelipeC1706 <jcarvajal@uniempresarial.edu.co>
2024-03-08 04:48:35 -0500

    somar

--- abogados/src/app/app-routing.module.ts
+++ abogados/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PeticionesComponent } from './componentes/peticiones/peticiones.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path: "peticiones", component: PeticionesComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- abogados/src/app/app.module.ts
+++ abogados/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './componentes/login/login.component';
+import { DashboardComponent } from './componentes/dashboard/dashboard.component';
+import { PeticionesComponent } from './componentes/peticiones/peticiones.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    LoginComponent
+    LoginComponent,
+    PeticionesComponent
   ],
   imports: [
     BrowserModule,
