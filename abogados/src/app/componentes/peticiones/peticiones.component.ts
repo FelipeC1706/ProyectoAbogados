aa4ee24ced90b3bd544999cf1aba7339951039d9
Santiago Jaimes Pinilla <sjaimes@uniempresarial.edu.co>
2024-03-20 20:05:17 -0500

    tabla

--- abogados/src/app/componentes/peticiones/peticiones.component.ts
+++ abogados/src/app/componentes/peticiones/peticiones.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-peticiones',
@@ -7,17 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeticionesComponent implements OnInit{
 
+  data: any;
+
   dtOptions: DataTables.Settings = {};
 
   constructor(){}
 
   ngOnInit(): void {
+    this.getPeticiones();
+
     this.dtOptions = {
+          columns: [
+            { title: 'Id', data: 'pet_id' },
+            { title: 'descripcion', data: 'pet_descripcion' },
+            { title: 'fecha registro', data: 'pet_fecha_registro' },
+            { title: 'fecha respuesta', data: 'pet_fecha_respuesta' },
+            { title: 'tipo peticion', data: 'tipo_pe_id' },
+            { title: 'documento abogado', data: 'abo_documento' },
+            { title: 'cliente', data: 'id_clientes' },
+            { title: 'seguimiento', data: 'seg_id' },
+            { title: 'prioridad', data: 'pet_prioridad' }
+          ],
           pagingType: 'full_numbers',
-          language: {
-            url: '//cdn.datatables.net/plug-ins/1.11.5/i18n/Spanish.json'
-          }
-    };
+        };
+        
   }
+
+  getPeticiones() {
+    fetch('http://localhost:3000/requests')
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Error en la operación fetch');
+        }
+      })
+      .then(data => {
+        this.data = data;
+        this.dtOptions.data = data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  
 
 }
