8c22c807a5ecc8911341dc00efc9f19032024578
Santyrc0325 <scamachor@uniempresarial.edu.co>
2024-03-31 19:55:12 -0500

    Fomulario actualizado
    Trae la infromacion de la peticion al formulario

--- abogados/src/app/componentes/actualizar/actualizar.component.ts
+++ abogados/src/app/componentes/actualizar/actualizar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-actualizar',
   templateUrl: './actualizar.component.html',
   styleUrls: ['./actualizar.component.css']
 })
-export class ActualizarComponent {
+export class ActualizarComponent implements OnInit {
+  item: any;
 
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.item = params;
+    });
+  }
 }
